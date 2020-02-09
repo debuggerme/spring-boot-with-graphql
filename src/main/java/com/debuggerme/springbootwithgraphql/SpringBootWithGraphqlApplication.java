6b8df741314cc70990a2e090610b8c7a62045ef5
Jamith Nimantha <jamithnimantha@gmail.com>
2020-02-09 17:41:45 +0530

    Initial Commit

---
+++ src/main/java/com/debuggerme/springbootwithgraphql/SpringBootWithGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.debuggerme.springbootwithgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/debuggerme/springbootwithgraphql/SpringBootWithGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debuggerme.springbootwithgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
